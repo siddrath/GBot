f75e1b004a2925ad0114e4107ed54ac3960954cc
gavynman25 <38404553+gavynman25@users.noreply.github.com>
2018-12-10 10:27:14 -0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -10,11 +10,165 @@ import random
 from discord.ext import commands
 from PIL import Image, ImageDraw, ImageFont
 
-bot = commands.Bot(description='BAsics can do a lot more.....', command_prefix=commands.when_mentioned_or('G.'))
+bot = commands.Bot(description='BAsics can do a lot more.....', command_prefix=commands.when_mentioned_or('f!'))
 
 
 class BAsics():
 
+    @commands.command()
+    async def owner(self, ctx):
+        ': Name of my creator'
+        await ctx.send('My owner is <@392337139309871106> ')
+        await ctx.message.delete()
+
+    @commands.command()
+    async def ping(self, ctx):
+        ': Check your connection '
+        t1 = ctx.message.created_at
+        m = await ctx.send('**Pong!**')
+        time = (m.created_at - t1).total_seconds() * 1000
+        await m.edit(content='**Pong! Took: {}ms**'.format(int(time)))
+        await ctx.message.delete()
+
+    @commands.command(pass_contex=True)
+    async def invite(self, ctx):
+        ': Invite me '
+        await ctx.send('https://discordapp.com/oauth2/authorize?client_id=394080286461263873&scope=bot&permissions=1543687243')
+
+    @commands.command()
+    async def uptime(self,ctx):
+        res = os.popen("uptime").read()
+        matches = re.findall(r"up (\d+) days, (\d+):(\d+)", res)
+        time = matches[0]
+        fmtime = "{0[0]} days, {0[1]} hours {0[2]} minutes".format(time)
+        await ctx.send(f'''```py\n{fmtime}```''')
+
+    @commands.command()
+    async def serverinfo(self, ctx):
+        ': Get the server info'
+        guild = ctx.guild
+        embed = discord.Embed(title=f'''{guild}''', colour=discord.Colour.dark_purple(), description='More Info Below', timestamp=datetime.datetime.utcnow())
+        embed.set_thumbnail(url=f'''{guild.icon_url}''')
+        embed.add_field(name='Server Created At :', value=f'''  {guild.created_at}''', inline=False)
+        embed.add_field(name='Created by :', value=f'''{guild.owner.mention}''', inline=False)
+        embed.add_field(name='Region :', value=f'''  {guild.region}''', inline=False)
+        embed.add_field(name='Server ID :', value=f'''{guild.id}''', inline=False)
+        embed.add_field(name='Server Members :', value=f'''  {len(guild.members)}''', inline=False)
+        embed.add_field(name='Online Members :',
+                        value=f'''{len([I for I in guild.members if I.status is discord.Status.online])}''',inline=False)
+        embed.add_field(name='Server Channel :', value=f'''  {len(guild.channels)}''', inline=False)
+        await ctx.send(embed=embed)
+
+    @commands.command(pass_context=True)
+    async def avatar(self, ctx, user: discord.Member = None):
+        """: Check AVATARS"""
+        user = user or ctx.message.author
+        embed = discord.Embed(title=f'''{user.name}'s Avatar''', description=f'''{user.name} looks like.....''',color=discord.Colour.dark_purple())
+        embed.set_image(url=user.avatar_url)
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def count(self, ctx):
+        ''': Get the info about my servers'''
+        total = sum(1 for m in set(ctx.bot.get_all_members()) if m.status != discord.Status.offline)
+        embed = discord.Embed(title=f'''Count''', colour=discord.Colour.dark_purple(),description=f'''I am in **{len(bot.guilds)}** servers \nI am used by **{len(bot.users)}** users \nI am currently entertaining **{total}** users''')
+
+        embed.set_thumbnail(url=f'''{bot.user.avatar_url}''')
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def profile(self, ctx, member: discord.Member = None):
+        ''': See your profile'''
+        member = member or ctx.message.author
+        x = Image.open("pngs/FBI.png")
+        x.load()
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get(member.avatar_url_as(format='png')) as r:
+                b = io.BytesIO(await r.read())
+        x1 = Image.open(b)
+        x1.load()
+        font_type = ImageFont.truetype('arialbd.ttf', 15)
+        font_type1 = ImageFont.truetype('arialbd.ttf', 14)
+        draw = ImageDraw.Draw(x)
+        x.paste(x1.resize((75, 75)), (195, 55))
+        draw.text(xy=(80, 166), text=member.name, fill=(0, 0, 0), font=font_type)
+        draw.text(xy=(75, 204), text=ctx.guild.name, fill=(0, 0, 0), font=font_type1)
+        draw.text(xy=(68, 223), text=member.top_role.name, fill=(0, 0, 0), font=font_type1)
+        x.save("profile.png")
+        await ctx.send(file=discord.File("profile.png"))
+        os.system("rm profile.png")
+
+    @commands.command()
+    async def wanted(self, ctx, member: discord.Member = None):
+        ''': Hunt someone'''
+        member = member or ctx.message.author
+        x = Image.open("pngs/Wanted.png")
+        x.load()
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get(member.avatar_url_as(format='png')) as r:
+                b = io.BytesIO(await r.read())
+        x1 = Image.open(b)
+        x3 = x.resize((400, 600))
+        x3.paste(x1.resize((300, 250)), (50, 160))
+        x3.save("wanted.png")
+        await ctx.send(file=discord.File("wanted.png"))
+        os.system("rm wanted.png")
+
+    @commands.command()
+    async def shit(self,ctx, member: discord.Member):
+        ''': Show em how shitty they are'''
+        x = Image.open("pngs/shit.png")
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get(member.avatar_url_as(format='png')) as r:
+                b = io.BytesIO(await r.read())
+        # open the pic and give it an alpha channel so it's transparent
+        im1 = Image.open(b).convert('RGBA')
+        im4 = im1.resize((120, 200))
+        # rotate it and expand it's canvas so the corners don't get cut off:
+        im2 = im4.rotate(-45, expand=1)
+
+        # note the second appearance of im2, that's necessary to paste without a bg
+        x.paste(im2, (200, 655), im2)
+        x.save("SHIT.png")
+        await ctx.send(file=discord.File("SHIT.png"))
+        os.system("rm SHIT.png")
+
+
+
+class BAmath():
+
+    @commands.command()
+    async def fact(self, ctx, num: int):
+        ': Get factorial of any number '
+        if num < 0:
+            await ctx.send('Sorry, factorial does not exist for negative numbers')
+        elif num == 0:
+            await ctx.send('The factorial of 0 is 1')
+        else:
+            from math import factorial
+            await ctx.send(f'''The factorial of {num} is : ```{factorial(num)}```''')
+    @commands.command()
+    async def add(self, ctx, num: int, num2: int):
+        ': Add two numbers'
+        await ctx.send(num + num2)
+
+    @commands.command()
+    async def factor(self, ctx, num: int):
+        ': Find prime factors '
+        await ctx.send('Factors are:')
+        i = 1
+        while i <= num:
+            k = 0
+            if (num % i) == 0:
+                j = 1
+                while j <= i:
+                    if (i % j) == 0:
+                        k = k + 1
+                    j = j + 1
+                if k == 2:
+                    await ctx.send('`1`')
+                    await ctx.send(f'''`{i}`''')
+            i = i + 1
 
 
 class BAdmin():
@@ -186,12 +340,13 @@ async def on_ready():
     bot.load_extension("fun")
     bot.load_extension("search")
     bot.load_extension('ExampleRepl')
-    options = ('help via G.help', 'to Gavyn S. ✓ᵛᵉʳᶦᶠᶦᵉᵈ#0981', f'on {len(bot.guilds)} servers')
+    options = ('help via f!help', 'to !👑!Fire Feathers!👑!#8265', f'on {len(bot.guilds)} servers')
     while True:
-        await bot.change_presence(activity=discord.Streaming(name=random.choice(options), url='https://www.twitch.tv/gavynlamar05'))
+        await bot.change_presence(activity=discord.Streaming(name=random.choice(options), url='https://www.twitch.tv/cohhcarnage'))
         await asyncio.sleep(10)
 
 
 bot.add_cog(BAdmin())
+bot.add_cog(BAmath())
 bot.add_cog(BAsics())
 bot.run(os.getenv('TOKEN'))
