72add7cd7b481bc120420a91dc1182863c864f55
gavynman25 <38404553+gavynman25@users.noreply.github.com>
2018-12-10 10:20:21 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,123 +15,217 @@ bot = commands.Bot(description='BAsics can do a lot more.....', command_prefix=c
 
 class BAsics():
 
-    @commands.command()
-    async def owner(self, ctx):
-        ': Name of my creator'
-        await ctx.send('My owner is <@392337139309871106> ')
-        await ctx.message.delete()
+@bot.command(aliases=['cmds'])
+async def commands(self, ctx):
+    member = ctx.author
+    embed = discord.Embed(title="Prefix", colour=discord.Colour.dark_blue(), description="G.")
+    embed.add_field(name='Commands', value='gbot \nserverinfo \nuserinfo \nhelp \njoined_at \nstats \nping \ninvme \navatar \npoll \nvote \nbug_report \nfeedback \nbbff')
+    embed.add_field(name='Admin/Mod Commands',value='ban \nkick \npurge \nswarn - soft warn \nwarn - reg. warn \n add_role \nmute \nunmute')
+    await ctx.send ('Check your :regional_indicator_d: :regional_indicator_m:')
+    await member.send(embed=embed)
 
-    @commands.command()
-    async def ping(self, ctx):
+
+@bot.command()
+async def gbot(self, ctx):
+    embed = discord.Embed(title="G Bot", colour=discord.Colour.dark_blue(),description="Created by: <@293800689266851850>", inline=False)
+    embed.set_thumbnail(url=f'''{bot.user.avatar_url}''')
+    embed.add_field(name='Contributor(s)', value="ir3#3333 \n------------ \nGarry#2508", inline=False)
+    embed.add_field(name='Version', value="1.0.0 [ALPHA]")
+    embed.set_footer(text = "Made with python 3.6.6", icon_url = 'https://cdn.discordapp.com/emojis/490607334876381204.png?v=1')
+    await ctx.send(embed=embed)
+
+
+@bot.command()
+async def serverinfo(self, ctx):
+    ': Get the server info'
+    guild = ctx.guild
+    embed = discord.Embed(title=f'''{guild}''', colour=discord.Colour.dark_blue(), description='More Info Below',
+                          timestamp=datetime.datetime.utcnow())
+    embed.set_thumbnail(url=f'''{guild.icon_url}''')
+    embed.add_field(name='Server Created At :', value=f'''  {guild.created_at}''', inline=False)
+    embed.add_field(name='Created by :', value=f'''{guild.owner}''', inline=False)
+    embed.add_field(name='Region :', value=f'''  {guild.region}''', inline=False)
+    embed.add_field(name='Server ID :', value=f'''{guild.id}''', inline=False)
+    embed.add_field(name='Server Members :', value=f'''  {len(guild.members)}''', inline=False)
+    embed.add_field(name='Online Members :',
+                    value=f'''{len([I for I in guild.members if I.status is discord.Status.online])}''', inline=False)
+    embed.add_field(name='Server Channel :', value=f'''  {len(guild.channels)}''', inline=False)
+    await ctx.send(embed=embed)
+
+
+@bot.command(aliases=['user', 'member', 'memberinfo',])
+async def userinfo(self, ctx, member: discord.Member = None):
+    name = "user",
+    aliases = 'member', 'memberinfo', 'userinfo'
+    if member is None:
+        member = ctx.author
+
+    e = discord.Embed(
+        title=f"User: {member.name}", colour=discord.Colour.blue(),
+        description=f"This is all the information I could find on {member.name}...",
+    )
+    e.set_thumbnail(
+        url=member.avatar_url_as(static_format="png")
+    )
+    e.add_field(
+        name="Name",
+        value=member.name
+    )
+    e.add_field(
+        name="Discriminator",
+        value=f"#{member.discriminator}"
+    )
+    e.add_field(
+        name="ID",
+        value=str(member.id)
+    )
+    e.add_field(
+        name="Bot",
+        value=str(member.bot).capitalize()
+    )
+    e.add_field(
+        name="Highest Role",
+        value=member.top_role.mention
+    )
+    e.add_field(
+        name="Join Position",
+        value=f"#{sorted(member.guild.members, key=lambda m: m.joined_at).index(member) + 1}"
+    )
+    e.add_field(
+        name="Created Account",
+        value=member.created_at.strftime("%c")
+    )
+    e.add_field(
+        name="Joined This Server",
+        value=member.joined_at.strftime("%c")
+    )
+    e.add_field(
+        name="Roles",
+        value=f"{len(member.roles) - 1} Roles: {', '.join([r.mention for r in member.roles if not r.is_default()])}"
+    )
+    await ctx.send(embed=e)
+
+#@bot.command()
+#async def help(self, ctx):
+    #embed=discord.Embed(title='So, you need help?', colour=discord.Colour.red(), description='[Support Discord](https://discord.gg/uHqmhgf)')
+    #embed.add_field(name='-----------', value='[Website](https://gbot.bubbleapps.io)')
+    #await ctx.send(embed=embed)
+
+@bot.command()
+async def stats(self, ctx):
+        ''': Get the info about my servers'''
+        total = sum(1 for m in set(ctx.bot.get_all_members()) if m.status != discord.Status.offline)
+        embed = discord.Embed(title=f'''Count''', colour=discord.Colour.dark_purple(),description=f'''I am in **{len(bot.guilds)}** servers \nI am used by **{len(bot.users)}** users \nI am currently entertaining **{total}** users''')
+        #markdown = ['![Discord Bots](https://discordbots.org/api/widget/493470893331447820.svg)](https://discordbots.org/bot/493470893331447820']
+        embed.set_thumbnail(url=f'''{bot.user.avatar_url}''')
+        await ctx.send(embed=embed)
+        #await ctx.send(f'''{markdown}''')
+
+@bot.command(pass_context=True)
+async def joined_at(self, ctx, member: discord.Member = None):
+    if member is None:
+        member = ctx.message.author
+        em = discord.Embed(title='Member', colour=discord.Colour.dark_red(),
+                            description=f'''{member} joined at {member.joined_at}''', timestamp=datetime.datetime.utcnow(), inline=False)
+        em.set_thumbnail(url=member.avatar_url)
+        await ctx.send(embed=em)
+
+@bot.command()
+async def ping(self, ctx):
         ': Check your connection '
         t1 = ctx.message.created_at
         m = await ctx.send('**Pong!**')
         time = (m.created_at - t1).total_seconds() * 1000
         await m.edit(content='**Pong! Took: {}ms**'.format(int(time)))
-        await ctx.message.delete()
 
-    @commands.command(pass_contex=True)
-    async def invite(self, ctx):
-        ': Invite me '
-        await ctx.send('https://discordapp.com/oauth2/authorize?client_id=394080286461263873&scope=bot&permissions=1543687243')
-
-    @commands.command()
-    async def uptime(self,ctx):
-        res = os.popen("uptime").read()
-        matches = re.findall(r"up (\d+) days, (\d+):(\d+)", res)
-        time = matches[0]
-        fmtime = "{0[0]} days, {0[1]} hours {0[2]} minutes".format(time)
-        await ctx.send(f'''```py\n{fmtime}```''')
-
-    @commands.command()
-    async def serverinfo(self, ctx):
-        ': Get the server info'
-        guild = ctx.guild
-        embed = discord.Embed(title=f'''{guild}''', colour=discord.Colour.dark_purple(), description='More Info Below', timestamp=datetime.datetime.utcnow())
-        embed.set_thumbnail(url=f'''{guild.icon_url}''')
-        embed.add_field(name='Server Created At :', value=f'''  {guild.created_at}''', inline=False)
-        embed.add_field(name='Created by :', value=f'''{guild.owner.mention}''', inline=False)
-        embed.add_field(name='Region :', value=f'''  {guild.region}''', inline=False)
-        embed.add_field(name='Server ID :', value=f'''{guild.id}''', inline=False)
-        embed.add_field(name='Server Members :', value=f'''  {len(guild.members)}''', inline=False)
-        embed.add_field(name='Online Members :',
-                        value=f'''{len([I for I in guild.members if I.status is discord.Status.online])}''',inline=False)
-        embed.add_field(name='Server Channel :', value=f'''  {len(guild.channels)}''', inline=False)
+@bot.command()
+async def perms(self, ctx, user: discord.Member = None):
+        ': Find what you can do on this server'
+        user = ctx.message.author if user is None else user
+        if not user:
+            user = ctx.author
+        mess = []
+        for i in user.guild_permissions:
+            if i[1]:
+                mess.append("\u2705 {}".format(i[0]))
+            else:
+                mess.append("\u274C {}".format(i[0]))
+        embed = discord.Embed(title = f'''{user.name} 's permissions in the server are: ''',description ="\n".join(mess), color = discord.Colour.dark_purple())
         await ctx.send(embed=embed)
 
-    @commands.command(pass_context=True)
-    async def avatar(self, ctx, user: discord.Member = None):
+@bot.command()
+async def invme(self, ctx):
+    embed=discord.Embed (title="So you want me huh?", colour=discord.Colour.dark_blue(), description='[Invite me](https://discordapp.com/api/oauth2/authorize?client_id=493470893331447820&scope=bot)')
+    await ctx.send (embed=embed)
+
+@bot.command(pass_context=True)
+async def avatar(self, ctx, user: discord.Member = None):
         """: Check AVATARS"""
         user = user or ctx.message.author
         embed = discord.Embed(title=f'''{user.name}'s Avatar''', description=f'''{user.name} looks like.....''',color=discord.Colour.dark_purple())
         embed.set_image(url=user.avatar_url)
         await ctx.send(embed=embed)
 
-    @commands.command()
-    async def count(self, ctx):
-        ''': Get the info about my servers'''
-        total = sum(1 for m in set(ctx.bot.get_all_members()) if m.status != discord.Status.offline)
-        embed = discord.Embed(title=f'''Count''', colour=discord.Colour.dark_purple(),description=f'''I am in **{len(bot.guilds)}** servers \nI am used by **{len(bot.users)}** users \nI am currently entertaining **{total}** users''')
+@bot.command()
+async def poll(self, ctx, *, poll_message):
+        embed = discord.Embed(title=f'''{ctx.author}'s new poll''', colour=discord.Colour.dark_blue(), description=poll_message)
+        try:
+            await ctx.message.delete()
+        except:
+            pass
+        msg = await ctx.send(embed=embed)        
+        try:
+            await msg.add_reaction("\N{THUMBS UP SIGN}")
+            await msg.add_reaction("\N{THUMBS DOWN SIGN}")
+        except:
+            await msg.delete()
+            await ctx.send("Make sure i can add reactions to the poll")
 
-        embed.set_thumbnail(url=f'''{bot.user.avatar_url}''')
-        await ctx.send(embed=embed)
+@bot.command()
+async def vote(self, ctx):
+    embed = discord.Embed(title='Vote for me!', colour=discord.Colour.blue(), description='[Vote Here](https://discordbots.org/bot/493470893331447820/vote)')
+    embed.set_thumbnail(url=f'''{bot.user.avatar_url}''')
+    embed.set_footer(text='Thanks for considering voting! - Gavyn S. ✓ᵛᵉʳᶦᶠᶦᵉᵈ#0981')
+    await ctx.send(embed=embed)
 
-    @commands.command()
-    async def profile(self, ctx, member: discord.Member = None):
-        ''': See your profile'''
-        member = member or ctx.message.author
-        x = Image.open("pngs/FBI.png")
-        x.load()
-        async with aiohttp.ClientSession() as cs:
-            async with cs.get(member.avatar_url_as(format='png')) as r:
-                b = io.BytesIO(await r.read())
-        x1 = Image.open(b)
-        x1.load()
-        font_type = ImageFont.truetype('arialbd.ttf', 15)
-        font_type1 = ImageFont.truetype('arialbd.ttf', 14)
-        draw = ImageDraw.Draw(x)
-        x.paste(x1.resize((75, 75)), (195, 55))
-        draw.text(xy=(80, 166), text=member.name, fill=(0, 0, 0), font=font_type)
-        draw.text(xy=(75, 204), text=ctx.guild.name, fill=(0, 0, 0), font=font_type1)
-        draw.text(xy=(68, 223), text=member.top_role.name, fill=(0, 0, 0), font=font_type1)
-        x.save("profile.png")
-        await ctx.send(file=discord.File("profile.png"))
-        os.system("rm profile.png")
+@bot.command()
+async def feedback(self, ctx, * , feedback):
+    channel = bot.get_channel(515220018997231676)
+    embed = discord.Embed(title="Feedback Submission :robot:", colour=discord.Colour.red(), description=f'''Submitted by- {ctx.author}''')
+    embed.add_field(name="Feedback", value=feedback, inline=False)
+    embed.set_footer(text=f"From {ctx.guild.name} ({ctx.guild.id})")
+    await channel.send(embed=embed)
+    await ctx.send("Your Feedback Has Been Submitted")
 
-    @commands.command()
-    async def wanted(self, ctx, member: discord.Member = None):
-        ''': Hunt someone'''
-        member = member or ctx.message.author
-        x = Image.open("pngs/Wanted.png")
-        x.load()
-        async with aiohttp.ClientSession() as cs:
-            async with cs.get(member.avatar_url_as(format='png')) as r:
-                b = io.BytesIO(await r.read())
-        x1 = Image.open(b)
-        x3 = x.resize((400, 600))
-        x3.paste(x1.resize((300, 250)), (50, 160))
-        x3.save("wanted.png")
-        await ctx.send(file=discord.File("wanted.png"))
-        os.system("rm wanted.png")
+@bot.command()
+async def bug_report(self, ctx, * , feedback):
+    channel = bot.get_channel(515220018997231676)
+    embed = discord.Embed(title="Bug Submission :robot:", colour=discord.Colour.red(), description=f'''Submitted by- {ctx.author}''')
+    embed.add_field(name="Feedback", value=feedback, inline=False)
+    embed.set_footer(text=f"From {ctx.guild.name} ({ctx.guild.id})")
+    await channel.send(embed=embed)
+    await ctx.send("Your Feedback Has Been Submitted")
 
-    @commands.command()
-    async def shit(self,ctx, member: discord.Member):
-        ''': Show em how shitty they are'''
-        x = Image.open("pngs/shit.png")
-        async with aiohttp.ClientSession() as cs:
-            async with cs.get(member.avatar_url_as(format='png')) as r:
-                b = io.BytesIO(await r.read())
-        # open the pic and give it an alpha channel so it's transparent
-        im1 = Image.open(b).convert('RGBA')
-        im4 = im1.resize((120, 200))
-        # rotate it and expand it's canvas so the corners don't get cut off:
-        im2 = im4.rotate(-45, expand=1)
 
-        # note the second appearance of im2, that's necessary to paste without a bg
-        x.paste(im2, (200, 655), im2)
-        x.save("SHIT.png")
-        await ctx.send(file=discord.File("SHIT.png"))
-        os.system("rm SHIT.png")
+
+@bot.command()
+async def servers(self, ctx):
+    a = []
+    for i in bot.guilds:
+        a.append(i.name)
+        await ctx.send(", ".join(a))
+
+
+@bot.command()
+async def bbff(self, ctx):
+    embed=discord.Embed(title='My Best Bot Friends Forever', colour=discord.Colour.red(), description='Add them to your server!')
+    embed.add_field(name='PewDiePie#7718', value='[Invite Here](https://discordapp.com/oauth2/authorize?client_id=508143906811019269&scope=bot&permissions=2146958847)')
+    embed.add_field(name='T-Series#7576', value='[Invite Here](https://discordapp.com/oauth2/authorize?client_id=500868806776979462&scope=bot&permissions=72710)')
+    embed.add_field(name='Touka#9248', value='[Invite Here](https://discordapp.com/oauth2/authorize?client_id=486093523024609292&scope=bot&permissions=2146958591)')
+    embed.add_field(name='Fusion#2584', value='[Invite Here](https://discordapp.com/api/oauth2/authorize?client_id=469204895946244106&permissions=8&scope=bot)')
+    embed.set_footer(text='Sincerely, Gavyn S. ✓ᵛᵉʳᶦᶠᶦᵉᵈ#0981', icon_url = 'https://cdn.discordapp.com/emojis/519688994120794132.png?v=1')
+    await ctx.send (embed=embed)
+
 
 
 class BAdmin():
